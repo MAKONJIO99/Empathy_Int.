ec52f4c6f777fc02b6eb739076c4541de2657981
MAKONJIO99 <francismakonjio2@gmail.com>
2021-04-27 00:56:08 +0300

    add the html files with their coresponding css and js files with a fully baked profile html

---
+++ js/profile.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#sname").mouseover(function(){
+      $("#overlaysponsor").show();
+    }).mouseout(function(){
+      $("#overlaysponsor").hide();
+    });
+  });
