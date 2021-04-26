67326170c5e1dc7de12e37321bac31d0e8bc82c8
Rubyroy12 <ibrakipz7@gmail.com>
2021-04-27 01:45:16 +0300

    add sponsor and beneficiary form

--- js/account.js
+++ js/account.js
@@ -1,8 +1,30 @@
 $(document).ready(function () {
     $("form#myform").submit(function (event) {
        event.preventDefault()
-        var user = $('input[type="radio"]:checked').val();
-        $("#user1").text(user);
+        var user = $('input[name="person"]:checked').val();
+        
         // alert("you selected" + user);
+        if (user=="Sponsor"){
+            $("form#sform").show();
+            $("p#user1").html(user).val();
+            $(".card>#bform").hide();
+        }else if(user=="Beneficiary"){
+            $(".card>#bform").show();
+            $(".user2").html(user).val();
+            $("form#sform").hide();
+            
+        }else{
+            alert("Select to proceed");
+
+        }
+    })
+})
+
+
+
+
+$(document).ready(function(){
+    $("form#sform").submit(function(){
+        // $("#").;
     })
 })
